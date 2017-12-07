a62d9f12041a2fd6a52339491f117fcc4dfb8b90
Dwayne M Pate <dwgames@gmail.com>
2017-12-06 22:32:15 -0600

    Created game and publisher model. Scaffolded controllers for each

---
+++ LetsRollApi/Controllers/PublishersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LetsRollApi.Models;
+
+namespace LetsRollApi.Controllers
+{
+    public class PublishersController : ApiController
+    {
+        private LetsRollApiContext db = new LetsRollApiContext();
+
+        // GET: api/Publishers
+        public IQueryable<Publisher> GetPublishers()
+        {
+            return db.Publishers;
+        }
+
+        // GET: api/Publishers/5
+        [ResponseType(typeof(Publisher))]
+        public async Task<IHttpActionResult> GetPublisher(int id)
+        {
+            Publisher publisher = await db.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(publisher);
+        }
+
+        // PUT: api/Publishers/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPublisher(int id, Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != publisher.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(publisher).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Publishers
+        [ResponseType(typeof(Publisher))]
+        public async Task<IHttpActionResult> PostPublisher(Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Publishers.Add(publisher);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = publisher.Id }, publisher);
+        }
+
+        // DELETE: api/Publishers/5
+        [ResponseType(typeof(Publisher))]
+        public async Task<IHttpActionResult> DeletePublisher(int id)
+        {
+            Publisher publisher = await db.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            db.Publishers.Remove(publisher);
+            await db.SaveChangesAsync();
+
+            return Ok(publisher);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return db.Publishers.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ LetsRollApi/Migrations/Configuration.cs
@@ -0,0 +1,70 @@
+namespace LetsRollApi.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using LetsRollApi.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LetsRollApi.Models.LetsRollApiContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LetsRollApi.Models.LetsRollApiContext context)
+        {
+            // protected override void Seed(BookService.Models.BookServiceContext context)
+  
+            context.Publishers.AddOrUpdate(x => x.Id,
+                new Publisher() { Id = 1, Name = "Mayfair Games" },
+                new Publisher() { Id = 2, Name = "Rio Grande Games" },
+                new Publisher() { Id = 3, Name = "Fantasy Flight Games" }
+                );
+
+            context.Games.AddOrUpdate(x => x.Id,
+                new Game()
+                {
+                    Id = 1,
+                    MinPlayers = 3,
+                    MaxPlayers = 4,
+                    PlayTime = 90,
+                    Age = 12,
+                    PublisherId = 1
+                },
+                new Game()
+                {
+                    Id = 2,
+                    MinPlayers = 2,
+                    MaxPlayers = 5,
+                    PlayTime = 60,
+                    Age = 10,
+                    PublisherId = 2
+                },
+                new Game()
+                {
+                    Id = 3,
+                    MinPlayers = 3,
+                    MaxPlayers = 6,
+                    PlayTime = 240,
+                    Age = 14,
+                    PublisherId = 3
+                }
+            );
+        }
+        //  This method will be called after migrating to the latest version.
+
+        //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+        //  to avoid creating duplicate seed data. E.g.
+        //
+        //    context.People.AddOrUpdate(
+        //      p => p.FullName,
+        //      new Person { FullName = "Andrew Peters" },
+        //      new Person { FullName = "Brice Lambson" },
+        //      new Person { FullName = "Rowan Miller" }
+        //    );
+        //
+    }
+}
+
