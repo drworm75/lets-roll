4cd07df0475d19c20613006eff48407ce9b8444b
Dwayne M Pate <dwgames@gmail.com>
2017-12-12 19:57:38 -0600

    added models for Session and Players

---
+++ LetsRollApi/Models/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LetsRollApi.Models
+{
+    public class Player
+    {   
+        public int Id { get; set; }
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+        public bool Win { get; set; }
+
+        // Foreign Key
+        public int SessionId { get; set; }
+        // Navigation property
+        public Session Session { get; set; }
+
+        // Foreign Key
+        //public int UserId { get; set; }
+        // Navigation property
+        //public User User { get; set; }
+
+    }
+}
---
+++ LetsRollApi/Models/Session.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace LetsRollApi.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+
+        //Foreign Key
+        public int GameId { get; set; }
+        //Navigation Property
+        public Game Game { get; set; }
+
+        //Foreign Key
+        //public int UserId { get; set; }
+        //Navigation Property
+        //public User User { get; set; }
+
+
+    }
+}
