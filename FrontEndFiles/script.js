6fa0ff2ccc1a0efdbee1501e350628502c88393c
Dwayne M Pate <dwgames@gmail.com>
2017-12-16 12:45:19 -0600

    printing games and text boxes for new games to page.

--- FrontEndFiles/script.js
+++ FrontEndFiles/script.js
@@ -3,15 +3,15 @@ var testApp = angular.module('testApp', []);
 testApp.controller('testController' , function ($scope, $http) {
     $scope.home = "This is the homepage";
     
-    $scope.getRequest = function () {
-        console.log("I've been pressed!");  
+    getRequest = function () { 
         $http.get("http://localhost:54854/api/Games")
             .then(function successCallback(response){
                 $scope.response = response;
-                console.log(response);
             }, function errorCallback(response){
                 console.log("Unable to perform get request");
             });
     };
+
+    getRequest()
     
 });
