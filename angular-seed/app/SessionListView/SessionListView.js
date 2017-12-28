89c4bcf9c9ec82e38bca770a1cdb30bd82b8787a
Dwayne Pate <dwgames@gmail.com>
2017-12-27 18:30:52 -0800

    Added inputs for adding a new game

---
+++ angular-seed/app/SessionListView/SessionListView.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('myApp.SessionListView', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/SessionListView', {
+    templateUrl: 'SessionListView/SessionListView.html',
+    controller: 'SessionListViewCtrl'
+  });
+}])
+
+.controller('SessionListViewCtrl', function($scope, $http) {
+    $scope.intro = "List of Game Sessions";
+    
+    $scope.getRequest = function () {
+        console.log("I've been pressed!");  
+        $http.get("http://localhost:54854/api/Games")
+        .then(function successCallback(response){
+            $scope.response = response;
+            console.log(response);
+        }, function errorCallback(response){
+            console.log("Unable to perform get request");
+        });
+     };
+
+     $scope.getRequest();
+ 
+});
