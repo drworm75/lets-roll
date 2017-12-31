e4442bef58168662e7fcf13719cdfffe72d00ecc
Dwayne Pate <dwgames@gmail.com>
2017-12-30 20:50:16 -0800

    game titles on sessionview link to gamedetailview.  GameDetailView now shows data for a each game.

--- angular-seed/app/GameDetailView/GameDetailView.js
+++ angular-seed/app/GameDetailView/GameDetailView.js
@@ -3,12 +3,27 @@
 angular.module('myApp.GameDetailView', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/GameDetailView/:name', {
+  $routeProvider.when('/GameDetailView/:id', {
     templateUrl: 'GameDetailView/GameDetailView.html',
     controller: 'GameDetailViewCtrl'
   });
 }])
 
-.controller('GameDetailViewCtrl', function($scope, $http) {
-    $scope.intro = "Details of Game {{name}}";
+.controller('GameDetailViewCtrl', function($scope, $http, $routeParams) {
+	var id = $routeParams.id
+    $scope.intro = "Details for";
+    console.log("$routeParams.id", id);
+
+    $scope.getDetails = function (id) {
+        console.log("Getting Details");  
+        $http.get(`http://localhost:54854/api/Games/${id}`)
+        .then(function successCallback(response){
+            $scope.gameInfo = response.data;
+            console.log(response);
+        }, function errorCallback(response){
+            console.log("Unable to perform get request");
+        });
+     };
+
+     $scope.getDetails(id);
 });
